2c73d0366b0d7283c49cb6cb720174934e0b59fc
Johnathan <Johnathan@John-PC>
2014-11-05 10:01:22 -0500

    help_desk

---
+++ app/controllers/HelpDeskController.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of HelpDeskController
+ *
+ * @author Johnathan
+ */
+class HelpDeskController extends BaseController {
+    //put your code here
+}
